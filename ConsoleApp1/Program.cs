2cb02064af401d69cd8610c9d65ca85bae72d60c
Ronnie Lawson <ronnie.lawson@esendex.com>
2018-09-17 10:10:02 +0100

    meh

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using GameofLifeDojo;
+using static System.Int32;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var columns = 0;
+            var rows = 0;
+            var seeds = 0;
+            var truechar = 'X';
+            var falsechar = '-';
+            if (args.Length > 0)
+            {
+                columns = Parse(args[0]);
+                rows = Parse(args[1]);
+                seeds = Parse(args[2]);
+            }
+
+            if (args.Length > 3)
+            {
+                truechar = args[3].ToCharArray()[0];
+                falsechar = args[4].ToCharArray()[0];
+            }
+
+            if (columns <= 0 || rows <= 0 || seeds <= 0) return;
+
+            var lifeGrid = new LifeGrid(columns, rows);
+
+            //seed
+            for (var i = seeds; i > 0; i--)
+            {
+                Random rnd = new Random(DateTime.Now.Millisecond);
+
+                var x = rnd.Next(columns);
+                var y = rnd.Next(rows);
+
+                while (lifeGrid.GetCell(x, y))
+                {
+
+                    x = rnd.Next(columns);
+                    y = rnd.Next(rows);
+                }
+
+                lifeGrid.Seed(x, y);
+            }
+            DisplayGrid(lifeGrid, truechar, falsechar);
+            while (true) 
+            {
+                Console.WriteLine("Press Enter to generate next generation");
+                var line = Console.ReadLine(); 
+                if (line == "exit") 
+                {
+                    break;
+                }
+
+                lifeGrid.Generate();
+                DisplayGrid(lifeGrid, truechar, falsechar);
+            }
+        }
+
+        private static void DisplayGrid(LifeGrid lifeGrid, char truechar, char falsechar)
+        {
+            foreach(string row in lifeGrid.GetRows(truechar,falsechar))
+            Console.WriteLine(row);
+        }
+    }
+}
---
+++ LifeTest/LifeTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using NUnit.Framework;
+
+namespace GameofLifeDojo
+{
+    [TestFixture]
+    class GivenA2x2Grid
+    {
+        class WhenSeedingWithOneCell
+        {
+            LifeGrid lifeGrid = new LifeGrid(2, 2);
+            private bool[,] _result;
+
+            [SetUp]
+            public void WhenGeneratingNextGeneration()
+            {
+                lifeGrid.Seed(0, 0);
+                _result = lifeGrid.Generate();
+            }
+
+            [Test]
+            public void ThenTheResultGridIsAsExpected()
+            {
+                Assert.IsFalse(_result[0,0]);
+                Assert.IsFalse(_result[0, 1]);
+                Assert.IsFalse(_result[1, 0]);
+                Assert.IsFalse(_result[1, 1]);
+            }
+        }
+
+        class WhenSeedingWithTwoCells
+        {
+            LifeGrid lifeGrid = new LifeGrid(2, 2);
+            private bool[,] _result;
+
+            [SetUp]
+            public void WhenGeneratingNextGeneration()
+            {
+                lifeGrid.Seed(0, 0);
+                lifeGrid.Seed(0, 1);
+                _result = lifeGrid.Generate();
+            }
+
+            [Test]
+            public void ThenTheResultGridIsAsExpected()
+            {
+                Assert.IsFalse(_result[0, 0]);
+                Assert.IsFalse(_result[0, 1]);
+                Assert.IsFalse(_result[1, 0]);
+                Assert.IsFalse(_result[1, 1]);
+            }
+        }
+
+        class WhenSeedingWithThreeCells
+        {
+            LifeGrid lifeGrid = new LifeGrid(2, 2);
+            private bool[,] _result;
+
+            [SetUp]
+            public void WhenGeneratingNextGeneration()
+            {
+                lifeGrid.Seed(0, 0);
+                lifeGrid.Seed(0, 1);
+                lifeGrid.Seed(1,0);
+                _result = lifeGrid.Generate();
+            }
+
+            [Test]
+            public void ThenTheResultGridIsAsExpected()
+            {
+                Assert.IsTrue(_result[0, 0]);
+                Assert.IsTrue(_result[0, 1]);
+                Assert.IsTrue(_result[1, 0]);
+                Assert.IsTrue(_result[1, 1]);
+            }
+        }
+
+        class WhenSeedingWithFourCells
+        {
+            LifeGrid lifeGrid = new LifeGrid(2, 2);
+            private bool[,] _result;
+
+            [SetUp]
+            public void WhenGeneratingNextGeneration()
+            {
+                lifeGrid.Seed(0, 0);
+                lifeGrid.Seed(0, 1);
+                lifeGrid.Seed(1, 0);
+                lifeGrid.Seed(1, 1);
+                _result = lifeGrid.Generate();
+            }
+
+            [Test]
+            public void ThenTheResultGridIsAsExpected()
+            {
+                Assert.IsTrue(_result[0, 0]);
+                Assert.IsTrue(_result[0, 1]);
+                Assert.IsTrue(_result[1, 0]);
+                Assert.IsTrue(_result[1, 1]);
+            }
+        }
+    }
+
+}
